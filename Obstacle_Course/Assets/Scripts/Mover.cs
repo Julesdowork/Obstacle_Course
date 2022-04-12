bf3a185961ec64da2dfc0a6409a1fa61f017aada
Julian McNeill <julesdowork@gmail.com>
2022-04-12 18:56:25 -0400

    Practicing With Methods

--- Obstacle_Course/Assets/Scripts/Mover.cs
+++ Obstacle_Course/Assets/Scripts/Mover.cs
@@ -9,14 +9,27 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    private void PrintInstructions()
+    {
+        Debug.Log("Welcome to the Obstacle Course!");
+        Debug.Log("Use the arrow keys (or WASD keys) to move Mr. Bean.");
+        Debug.Log("Make your way to the end of the course, making sure to avoid the obstacles along the way...");
+    }
+
+    private void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
         transform.Translate(xValue, 0, zValue);
     }
 }
